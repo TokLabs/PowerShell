e638d07a12295c5a1a3adf03ca57243677d82df5
Tok <tom.beavill@protonmail.com>
2021-09-26 23:13:11 +0100

    additions
    Added comment based help.
    Modified regex.

--- Deobfuscate-O365SafeLink.ps1
+++ Deobfuscate-O365SafeLink.ps1
@@ -1,4 +1,22 @@
 function Deobfuscate-O365SafeLink{
+	<#
+	.SYNOPSIS
+	Deobfuscates Office 365 safe links.
+	
+	.DESCRIPTION
+	
+	
+	.PARAMETER uri
+	Specifies a safe link to deobfuscate.
+	
+	.EXAMPLE
+	Deobfuscate-O365SafeLink -uri "https://"
+	
+	.LINK
+	https://github.com/TokLabs/Powershell/blob/master/Deobfuscate-O365SafeLink.ps1
+	#>
+	
 	[CmdletBinding()] param(
 		[parameter(Position = 0, Mandatory=$true)][string]$uri)
-	[uri]::UnescapeDataString((($uri -replace "https://[a-zA-Z]{3}[0-9]{2}.safelinks.protection.outlook.com/\?url=","").split("&data"))[0])}
+		
+	[uri]::UnescapeDataString((($uri -replace "https://[a-zA-Z]{2,3}[0-9]{2}.safelinks.protection.outlook.com/\?url=","").split("&data"))[0])}
