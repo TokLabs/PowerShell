7b5dead5c0aac11afb1cceabdaff6616b87d7f90
Tok <tom.beavill@protonmail.com>
2021-09-26 23:34:21 +0100

    Added example and comments.

--- Deobfuscate-O365SafeLink.ps1
+++ Deobfuscate-O365SafeLink.ps1
@@ -10,7 +10,7 @@ function Deobfuscate-O365SafeLink{
 	Specifies a safe link to deobfuscate.
 	
 	.EXAMPLE
-	Deobfuscate-O365SafeLink -uri "https://"
+	Deobfuscate-O365SafeLink -uri "https://eur03.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.toklabs.com&data=firstname.lastname%40domain.com&sdata=xxxx&reserved=0"
 	
 	.LINK
 	https://github.com/TokLabs/Powershell/blob/master/Deobfuscate-O365SafeLink.ps1
@@ -19,4 +19,6 @@ function Deobfuscate-O365SafeLink{
 	[CmdletBinding()] param(
 		[parameter(Position = 0, Mandatory=$true)][string]$uri)
 		
+	<#String is extracted through regex and spliting.
+		Extracted string is decoded with UnescapeDataString method#>	
 	[uri]::UnescapeDataString((($uri -replace "https://[a-zA-Z]{2,3}[0-9]{2}.safelinks.protection.outlook.com/\?url=","").split("&data"))[0])}
