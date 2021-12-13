7601a9ab57a084e3754c4eaa673f8a3e996549a6
TokLabs <65448297+TokLabs@users.noreply.github.com>
2021-12-13 00:34:08 +0000

    Switched back to Pascal case. Added description.

--- ConvertFrom-Base64.ps1
+++ ConvertFrom-Base64.ps1
@@ -4,7 +4,7 @@ function ConvertFrom-Base64{
 	Converts a plaintext string from Base64.
 	
 	.DESCRIPTION
-	
+	Powershell cmdlet to convert one or more Base64 strings and output to a table. This cmdlet can convert both UTF8 and UTF-16LE(Powershell's Base64 encoding)
 	
 	.PARAMETER string
 	Mandatory, specifies atleast 1 string to convert from Base64.
@@ -16,45 +16,45 @@ function ConvertFrom-Base64{
 	ConvertFrom-Base64 c3RyaW5nMQ==
 	
 	.EXAMPLE
-	ConvertFrom-Base64 -string c3RyaW5nMQ==
+	ConvertFrom-Base64 -String c3RyaW5nMQ==
 	
 	.EXAMPLE
-	ConvertFrom-Base64 -string c3RyaW5nMQ==,c3RyaW5nMg==
+	ConvertFrom-Base64 -String c3RyaW5nMQ==,c3RyaW5nMg==
 	
 	.EXAMPLE
-	ConvertFrom-Base64 -string dABlAHMAdAA= -unicode
+	ConvertFrom-Base64 -String dABlAHMAdAA= -Unicode
 	
 	.LINK
 	https://github.com/TokLabs/Powershell/blob/master/ConvertFrom-Base64.ps1
 	#>
 	
 	[CmdletBinding()] Param(
-		[parameter(Position = 0, Mandatory=$True)][string[]]$string,
-		[parameter()][switch]$unicode)
+		[parameter(Position = 0, Mandatory=$True)][string[]]$String,
+		[parameter()][switch]$Unicode)
 		
-	if ($unicode -eq $true){
-		$format = "unicode"}
+	if ($Unicode -eq $True){
+		$Format = "unicode"}
 	else{
-		$format = "UTF8"}
-	Write-Verbose "format: $format"
-	$stringCount = $string.count
-	Write-Verbose "Amount of items in string: $stringCount"
+		$Format = "UTF8"}
+	Write-Verbose "format: $Format"
+	$StringCount = $String.count
+	Write-Verbose "Amount of items in string: $StringCount"
 	$i = [int]0
-	foreach($_ in $string){
+	foreach($_ in $String){
 		$i++	
 		try{
-			$encodedString = $_
+			$EncodedString = $_
 			Write-Verbose "String: $i"
-			Write-Verbose "Encoded string: $encodedString"
-			$value = [Convert]::FromBase64String($encodedString)
-			Write-Verbose "Value: $value"
-			$plainTextString = [System.Text.Encoding]::$format.GetString($value)
-			$outputTable = New-Object -TypeName psobject
-		$outputTable | Add-Member -MemberType NoteProperty -name PlainText -value $plainTextString
-		$outputTable | Add-Member -MemberType NoteProperty -name Base64 -value $encodedString}
+			Write-Verbose "Encoded string: $EncodedString"
+			$value = [Convert]::FromBase64String($EncodedString)
+			Write-Verbose "Value: $Value"
+			$PlainTextString = [System.Text.Encoding]::$Format.GetString($value)
+			$OutputTable = New-Object -TypeName psobject
+		$OutputTable | Add-Member -MemberType NoteProperty -name PlainText -value $PlainTextString
+		$OutputTable | Add-Member -MemberType NoteProperty -name Base64 -value $EncodedString}
 		catch{
-			$outputTable = New-Object -TypeName psobject
-		$outputTable | Add-Member -MemberType NoteProperty -name PlainText -value "Invalid string"
-		$outputTable | Add-Member -MemberType NoteProperty -name Base64 -value $encodedString}
-		$outputTable}
+			$OutputTable = New-Object -TypeName psobject
+		$OutputTable | Add-Member -MemberType NoteProperty -name PlainText -value "Invalid string"
+		$OutputTable | Add-Member -MemberType NoteProperty -name Base64 -value $EncodedString}
+		$OutputTable}
 	}
