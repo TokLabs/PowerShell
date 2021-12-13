cd68ad6c4ac71cdf5a358200e7095675c260e20c
TokLabs <65448297+TokLabs@users.noreply.github.com>
2021-12-13 00:57:36 +0000

    Corrected and changed back to Pascal case.

--- Deobfuscate-O365SafeLink.ps1
+++ Deobfuscate-O365SafeLink.ps1
@@ -4,21 +4,21 @@ function Deobfuscate-O365SafeLink{
 	Deobfuscates Office 365 safe links.
 	
 	.DESCRIPTION
+	Powershell cmdlet to debobfuscate Office 365's safe links.
 	
-	
-	.PARAMETER uri
+	.PARAMETER Uri
 	Specifies a safe link to deobfuscate.
 	
 	.EXAMPLE
-	Deobfuscate-O365SafeLink -uri "https://eur03.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.toklabs.com&data=firstname.lastname%40domain.com&sdata=xxxx&reserved=0"
+	Deobfuscate-O365SafeLink -Uri "https://eur03.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.toklabs.com&data=firstname.lastname%40domain.com&data=xxxx&reserved=0"
 	
 	.LINK
 	https://github.com/TokLabs/Powershell/blob/master/Deobfuscate-O365SafeLink.ps1
 	#>
 	
 	[CmdletBinding()] param(
-		[parameter(Position = 0, Mandatory=$true)][string]$uri)
+		[parameter(Position = 0, Mandatory=$True)][string]$Uri)
 		
 	<#String is extracted through regex and spliting.
 		Extracted string is decoded with UnescapeDataString method#>	
-	[uri]::UnescapeDataString((($uri -replace "https://[a-zA-Z]{2,3}[0-9]{2}.safelinks.protection.outlook.com/\?url=","").split("&data"))[0])}
+	[uri]::UnescapeDataString(($Uri -replace "https://[a-zA-Z]{2,3}[0-9]{2}.safelinks.protection.outlook.com/\?url=","") -split"&data").split(" ")[0]}
